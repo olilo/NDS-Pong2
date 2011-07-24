a253548ebff21dc29a39cefa26dbc5b6229d9b96
Oliver Lorenz <oliver-lorenz@gmx.de>
2011-07-24 20:52:58 +0200

    mode_server: more extensive debug of protocol test

--- source/mode_server.cpp
+++ source/mode_server.cpp
@@ -16,23 +16,41 @@
 // Methods
 
 void mode_server(void) {
+    consoleDemoInit();
+
+    // buffer for messaging
     char buffer[256];
     message message;
 
     // make a test of the protocol
-    const char * test_message = "$SACCP*0\n$garbage,ignore\n$SLOCA,1,,212,1,32,12,,*97\n";
+    const char * test_message = "$SACCP*66\n$garbage,ignore\n$SLOCA,1,,212,1,32,12,,*97\n";
+
+    printf("Test message is:\n%s", test_message);
+    printf("\n");
 
     strcpy(buffer, test_message);
 
     message = readMessage(0, buffer);
 
-    printf("Message with: status=%s, param count: %d, checksum: %d, restbuffer: %s", message.status, message.parameter_count, message.checksum, message.restbuffer);
+    printf("Got message: status: %s, param count: %d, checksum: %d\n", message.status, message.parameter_count, message.checksum);
+    printf("Parameters: ");
+    for (int i = 0; i < message.parameter_count; i++) printf("%d,", message.parameters[i]);
+    printf("\n\n");
 
     strcpy(buffer, message.restbuffer);
 
     message = readMessage(0, buffer);
 
-    printf("Message with: status=%s, param count: %d, checksum: %d, restbuffer: %s", message.status, message.parameter_count, message.checksum, message.restbuffer);
+    printf("Got message: status=%s, param count: %d, checksum: %d\n", message.status, message.parameter_count, message.checksum);
+    printf("Parameters: ");
+    for (int i = 0; i < message.parameter_count; i++) printf("%d,", message.parameters[i]);
+    printf("\n\n");
+
+    strcpy(buffer, message.restbuffer);
+
+    message = readMessage(0, buffer);
+
+    printf("Got message: status=%s, param count: %d, checksum: %d\n", message.status, message.parameter_count, message.checksum);
 
     // TODO make a connection to server, setup display etc.
 }
